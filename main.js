b81f2b3958a154ae63aed61c63f26c11f00fc28b
Ethan Jurman <ethanjurman@gmail.com>
2022-07-01 17:10:14 -0400

    game working

---
+++ main.js
@@ -0,0 +1,195 @@
+const SHAPES = ['cube'];
+const COLORS = ['color1', 'color2', 'color3', 'color4'];
+const GRID_WIDTH_SIZE = 8;
+const GRID_HEIGHT_SIZE = 8;
+
+let cursorPos = {x: 0, y: 0};
+let createHold = false;
+
+const getRandomShape = () => {
+  return SHAPES[Math.floor(Math.random() * SHAPES.length)];
+}
+
+const getRandomColor =
+() => {
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
+}
+
+const makeCell = (x, y) => {
+    const cell = document.createElement('div');
+    const color = getRandomColor();
+    const shape = getRandomShape();
+    cell.setAttribute('class', `cell ${color} ${shape}`);
+    cell.style = `left: ${54 * x}px; top: ${54 * y}px;`
+    cell.dataset.x = x;
+    cell.dataset.y = y;
+    cell.dataset.color = color;
+    cell.dataset.shape = shape;
+    return cell;
+} 
+
+const makeCursor = () => {
+  const cursor = document.createElement('div');
+  cursor.setAttribute('class', 'cursor');
+  return cursor;
+}
+
+const makeGrid = () => {
+  const grid = document.createElement('div');
+  grid.setAttribute('class', 'grid');
+  document.body.appendChild(grid);
+  grid.appendChild(makeCursor());
+
+  for (let i = 0; i < GRID_WIDTH_SIZE; i++) {
+    for (let j = 0; j < GRID_HEIGHT_SIZE; j++) {
+      grid.appendChild(makeCell(i, j))
+    }
+  }
+}
+
+makeGrid();
+
+document.onkeydown = (ev) => {
+  if (ev.key === 'w' || ev.key === 'ArrowUp') {
+    cursorPos = {x: cursorPos.x, y: Math.max(cursorPos.y - 1, 0)}
+  }
+  if (ev.key === 'a' || ev.key === 'ArrowLeft') {
+    cursorPos = {x: Math.max(cursorPos.x - 1, 0), y: cursorPos.y}
+  }
+  if (ev.key === 's' || ev.key === 'ArrowDown') {
+    cursorPos = {x: cursorPos.x, y: Math.min(cursorPos.y + 1, GRID_HEIGHT_SIZE - 2)}
+  }
+  if (ev.key === 'd' || ev.key === 'ArrowRight') {
+    cursorPos = {x: Math.min(cursorPos.x + 1, GRID_WIDTH_SIZE - 2), y: cursorPos.y}
+  }
+  if (ev.key === ' ') {
+    rotateCells();
+  }
+  updateCursor();
+  clearRows();
+}
+
+const updateCursor = () => {
+  const cursor = document.querySelector('.cursor');
+  const {x, y} = cursorPos;
+  cursor.style = `left: ${54 * x}px; top: ${54 * y}px;`
+}
+
+const getCellItem = (x,y) => {
+  return document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
+}
+
+const rotateCells = () => {
+  const {x, y}= cursorPos;
+  const cellsToRotate = [[x, y], [x + 1, y], [x + 1, y + 1], [x, y + 1]];
+  const cellElements = cellsToRotate.map((pair) => getCellItem(pair[0],pair[1]));
+  cellElements.map((cellElement, index) => {
+    const [newX, newY] = cellsToRotate[(Number(index) + 1) % cellsToRotate.length]
+    cellElement.dataset.x = newX;
+    cellElement.dataset.y = newY;
+    cellElement.style = `left: ${54 * newX}px; top: ${54 * newY}px;`
+  });
+}
+
+const clearColumns = () => {
+  let wereItemsRemoved = false;
+  for (let i = 0; i < GRID_WIDTH_SIZE; i++) {
+    let color = 'UNREGISTERED';
+    for (let j = 0; j < GRID_HEIGHT_SIZE; j++) {
+      const cellItem = getCellItem(i, j);
+      if (!cellItem) {
+        continue;
+      }
+      if (color === 'UNREGISTERED') {
+        color = cellItem.dataset.color;
+      }
+      if (color !== cellItem.dataset.color) {
+        color = null;
+      }
+    }
+    if (color && color !== 'UNREGISTERED') {
+      const cells = document.querySelectorAll(`[data-x="${i}"]`)
+      cells.forEach(cell => cell.classList.add('remove'));
+      setTimeout(() => {
+        cells.forEach(cell => cell.remove());
+      }, 100)
+      wereItemsRemoved = true;
+    }
+  }
+  return wereItemsRemoved;
+}
+
+const clearRows = () => {
+  let wereItemsRemoved = false;
+  for (let i = 0; i < GRID_WIDTH_SIZE; i++) {
+    let color = 'UNREGISTERED';
+    for (let j = 0; j < GRID_HEIGHT_SIZE; j++) {
+      const cellItem = getCellItem(j, i);
+      if (!cellItem) {
+        continue;
+      }
+      if (color === 'UNREGISTERED') {
+        color = cellItem.dataset.color;
+      }
+      if (color !== cellItem.dataset.color) {
+        color = null;
+      }
+    }
+    if (color && color !== 'UNREGISTERED') {
+      const cells = document.querySelectorAll(`[data-y="${i}"]`);
+      cells.forEach(cell => cell.classList.add('remove'));
+      setTimeout(() => {
+        cells.forEach(cell => cell.remove());
+      }, 100)
+      wereItemsRemoved = true;
+    }
+  }
+  return wereItemsRemoved;
+}
+
+const gravity = () => {
+  const cellElements = document.querySelectorAll('.cell')
+  for (cellElement of cellElements) {
+    const {x, y} = cellElement.dataset;
+    if (Number(y) === GRID_HEIGHT_SIZE - 1) {
+      continue;
+    }
+    if (!getCellItem(Number(x), Number(y) + 1)) {
+      cellElement.dataset.y = Number(y) + 1;
+      cellElement.style = `left: ${54 * x}px; top: ${54 * (Number(y) + 1)}px;`;
+    }
+  }
+}
+
+const makeTopCells = () => {
+  let wereItemsAdded = false;
+  const grid = document.querySelector('.grid');
+  for (let x = 0; x < GRID_WIDTH_SIZE; x++) {
+    if (!getCellItem(x, 0)) {
+      grid.appendChild(makeCell(x, 0));
+      wereItemsAdded = true;
+    }
+  }
+  return wereItemsAdded;
+}
+
+function step() {
+  gravity();
+  if (createHold) { // if create hold is true, allow clearing items
+    const wereItemsRemoved = clearRows() || clearColumns();
+    if (!wereItemsRemoved) {
+      createHold = false;
+    }
+  }
+  if (!createHold) { // if create hold is false, allow creating items
+    const wereItemsAdded = makeTopCells();
+    if (!wereItemsAdded) {
+      createHold = true;
+    }
+  }
+  setTimeout(() => {
+    window.requestAnimationFrame(step);
+  }, 100)
+}
+
+window.requestAnimationFrame(step);
